acb5bfbe031f8b4baf933e2f37e0464991b81623
Ilya Bibikov <bibikov.ilya@gmail.com>
2015-02-10 17:30:33 +0200

    Add next and prev slidings images

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,11 +1,37 @@
+var $window = $(window);
+
 var App = {
+  rail: {
+    focus: function(index) {
+      self.currentIndex = index;
+      console.log(index);
 
-}
+      var elementLeftPosition = parseInt($('#rail img:eq(' + index + ')').position().left, 10);
+      var centerPosition = (elementLeftPosition - ($window.width() * 0.5)) + $('#rail img:eq(' + index + ')').width() * 0.5;
 
-$(document).on('click', '#rail.rail-next, #rail-buttons .rail-next', function() {
-  TweenMax.to($('#rail img:first'), 0.3, {delay: 0.3, opacity: 0.3});
-  console.log('FUCK!');
-});
+      // TweenMax.to($('#rail img:eq('+currentIndex+')'), 0.3, {delay: 0.3, opacity: 0.3});
+      TweenMax.to($('#rail'), 0.8, {left: -centerPosition, ease: Expo.easeInOut});
+    },
 
-$(document).on('click', '#rail.rail-prev, #rail-buttons .rail-prev', function() {
+    nav: function() {
+      $(document).on('click', '#rail-buttons .rail-next', function() {
+        App.rail.focus(self.currentIndex + 1);
+      });
+
+      $(document).on('click', '#rail-buttons .rail-prev', function() {
+        App.rail.focus(self.currentIndex - 1);
+      });
+    },
+
+    start: function() {
+      self.currentIndex = 1;
+
+      App.rail.nav();
+      App.rail.focus(self.currentIndex);
+    }
+  }
+};
+
+$(window).load(function() {
+  App.rail.start();
 });
