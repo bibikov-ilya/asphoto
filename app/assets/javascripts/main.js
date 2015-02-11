21981fcf4c475b2def4b1b642974d89ff65ff452
Ilya Bibikov <bibikov.ilya@gmail.com>
2015-02-11 10:31:25 +0200

    Unactive sliding on boundary photos

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,16 +3,25 @@ var $window = $(window);
 var App = {
   rail: {
     focus: function(index) {
-      self.currentIndex = index;
-      console.log(index);
+      if ($('#rail img:eq(' + index + ')').length && index >= 0) {
+        self.currentIndex = index;
 
-      var elementLeftPosition = parseInt($('#rail img:eq(' + index + ')').position().left, 10);
-      var centerPosition = (elementLeftPosition - ($window.width() * 0.5)) + $('#rail img:eq(' + index + ')').width() * 0.5;
+        $('#rail-buttons span').removeClass('unactive');                    
+        if( index === 0 ) {
+            $('.rail-prev span').addClass('unactive');
+        }      
+        if( index+1 >= $('#rail img').length ) {
+            $('.rail-next span').addClass('unactive');
+        }
 
-      TweenMax.to($('#rail img:not(:eq(' + index + '))'), 0.3, {delay: 0.3, opacity: 0.3});
-      TweenMax.to($('#rail img:eq(' + index + ')'), 0.3, {delay: 0.3, opacity: 1});
+        var elementLeftPosition = parseInt($('#rail img:eq(' + index + ')').position().left, 10);
+        var centerPosition = (elementLeftPosition - ($window.width() * 0.5)) + $('#rail img:eq(' + index + ')').width() * 0.5;
 
-      TweenMax.to($('#rail'), 0.8, {left: -centerPosition, ease: Expo.easeInOut});
+        TweenMax.to($('#rail img:not(:eq(' + index + '))'), 0.3, {delay: 0.3, opacity: 0.3});
+        TweenMax.to($('#rail img:eq(' + index + ')'), 0.3, {delay: 0.3, opacity: 1});
+
+        TweenMax.to($('#rail'), 0.8, {left: -centerPosition, ease: Expo.easeInOut});
+      }
     },
 
     nav: function() {
